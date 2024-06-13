668fd94bc91010eeab742c72243e495be4c359a3
leapino <leandropinocorrea@gmail.com>
2024-06-13 18:45:30 -0300

    Add files via upload

---
+++ lab4/DTFecha.cpp
@@ -0,0 +1,67 @@
+#ifndef DTFECHA_CPP
+#define DTFECHA_CPP
+
+#include "declaraciones/DTFecha.h"
+
+DTFecha::DTFecha(){
+    this->dia=0;
+    this->mes=0;
+    this->anio=0;
+    this->hora=0;
+    this->minuto=0;
+    }
+
+DTFecha::DTFecha(int dia, int mes, int anio,int hora,int minuto){
+    this->dia = dia;
+    this->mes = mes;
+    this->anio = anio;
+    this->hora = hora;
+    this->minuto = minuto;
+}
+
+int DTFecha::getDia(){
+    return this->dia;
+}
+
+int DTFecha::getMes(){
+    return this->mes;
+}
+
+int DTFecha::getAnio(){
+    return this->anio;
+}
+
+int DTFecha::getHora(){
+    return this->hora;
+}
+
+int DTFecha::getMinuto(){
+    return this->minuto;
+}
+
+void DTFecha::setAnio(int anio){
+     this->anio = anio;
+}   
+
+void DTFecha::setDia(int dia){
+     this->dia = dia;
+}   
+
+void DTFecha::setMes(int mes){
+     this->mes = mes;
+}   
+
+void DTFecha::setHora(int hora){
+     this->hora = hora;
+}   
+
+void DTFecha::setMinuto(int minuto){
+     this->minuto = minuto;
+}   
+
+
+DTFecha::~DTFecha() {
+}
+
+
+#endif
