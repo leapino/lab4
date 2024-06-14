0e0a2407238e43af0d5946f3c7cdad752530d0ce
ThomasLlanes <thomas.llanes22@gmail.com>
2024-06-14 16:25:20 -0300

    se crean getters y setters de DTUsuario

--- lab4/declaraciones/DTUsuario.h
+++ lab4/declaraciones/DTUsuario.h
@@ -2,6 +2,7 @@
 #define DTUSUARIO_H
 #include "../declaraciones/DTCliente.h"
 #include "../declaraciones/DTVendedor.h"
+#include "../declaraciones/DTFecha.h"
 class DTUsuario {
     private:
     bool DTesCliente;
@@ -9,6 +10,24 @@ class DTUsuario {
     DTFecha DTNacimiento;
     DTCliente * pDTCliente; 
     DTVendedor * pDTVendedor;
+    public:
+    //Creadores
+    DTUsuario();
+    DTUsuario(bool esCliente, std::string Nickname, DTFecha fecha, DTCliente *cliente, DTVendedor *vendedor);
+    //Destructor
+    ~DTUsuario();
+    //Getters
+    bool getDTesCliente();
+    std::string getDTNickname();
+    DTFecha getDTNacimiento();
+    DTCliente * getpDTCliente();
+    DTVendedor * getpDTVendedor();
+    //Setters
+    void setDTesCliente(bool check);
+    void setDTNickname(std::string nick);
+    void setDTNacimiento(DTFecha fecha);
+    void setpDTCliente(DTCliente* cliente);
+    void setpDTVendedor(DTVendedor* vendedor);
 };
 
 #endif
