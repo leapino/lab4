272a7d881dca8933b5b1a732a8ea8791c5d8cbb4
miguelcarleo <miguecar99@gmail.com>
2024-06-20 17:43:14 -0300

    arreglo de errores

--- lab4/declaraciones/controladorProducto.h
+++ lab4/declaraciones/controladorProducto.h
@@ -23,8 +23,8 @@ class ControladorProducto {
         std::map<int, DT2Producto> listarProductos(std::string RUTVendedor); //Devuelve un set de DT2Producto con codigo y nombre asociado a los productos del vendedor
         //void agregarProducto(int codigo,int cantMin);
         void confirmarAltaPromocion(std::string,std::string,float,DTFecha,std::map<int, int>);
-        std::set<DTProducto*> getProductosDisp();
-        std::map<std::string,DTProducto*> getInfoProd(std::string producto);//Devuelve un DT del producto y en la casilla de string el nickname del vendedor
+        std::set<DTProducto> getProductosDisp();
+        std::map<std::string,DTProducto> getInfoProd(std::string producto);//Devuelve un DT del producto y en la casilla de string el nickname del vendedor
         std::map<int,std::string> getProds();//Retorna el codigo y el nombre de todos los productos
         bool hayStock(int codigoP,int cantidad);
         void prodEnCompra(std::map<int,int> prods);
@@ -33,7 +33,7 @@ class ControladorProducto {
         std::map<std::string, DTPromocion> getPromos();
         std::set<DTProducto> getProductoPromo(std::string);
         DTVendedor vendedorPromo(DTProducto);
-        void escribirCom(std::string comment,DTFecha* fecha,int codProd,Usuario* usuario,int idCom);
+        void escribirCom(std::string comment,DTFecha fecha,int codProd,std::string usuario,int idCom);
         std::list<DTPromocion*> getPromoVigente(std::string vendedor,DTFecha fechaActual);
 };
 
