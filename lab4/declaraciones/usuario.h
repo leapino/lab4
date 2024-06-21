e66a12b972d0adc462dbee039b26a0a06781dbaa
leapino <leandropinocorrea@gmail.com>
2024-06-21 18:21:50 -0300

    EliminarComentarios

--- lab4/declaraciones/usuario.h
+++ lab4/declaraciones/usuario.h
@@ -8,6 +8,7 @@
 #include <map>
 
 #include "DTFecha.h"
+#include "comentario.h"
 
 class Comentario;
 class Cliente;
@@ -18,27 +19,27 @@ class Usuario {
         std::string nickname;
         std::string password;
         DTFecha fechaNacimiento;
-        std::map<std::string,Comentario*> comentarios;
+        std::map<int,Comentario*> comentarios;
     public:
         //Creadores
         Usuario();
         Usuario(std::string nick, std::string pass, DTFecha fechnaci);
         //Getters
-        std::map<std::string,Comentario*> getComentarios();
+        std::map<int,Comentario*> getComentarios();
         std::string getNickname();
         std::string getPassword();
         DTFecha getFecha();
         Cliente* getCliente();
         Vendedor* getVend();
         //Setters
-        void setComentarios(std::map<std::string,Comentario*>);
+        void setComentarios(int,Comentario*);
         void setNickname(std::string);
         void setPassword(std::string);
         void setFecha(DTFecha);
         //Funciones
         virtual bool esVendedor();
         virtual bool esCliente();
-        std::map<std::string, std::string> listarComentarios();
-        void borrarComentario(std::string id);
+        std::map<int, std::string> listarComentarios();
+        void borrarComentario(int id);
 };
 #endif
